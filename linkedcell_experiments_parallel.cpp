8d64db97fd5725555dd1c1f29e8fe71ea1bdb250
amartyads <amartyads@gmail.com>
2025-07-25 10:32:55 +0200

    separation of header

---
+++ linkedcell_experiments_parallel.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <random>
+
+#include <Kokkos_Core.hpp>
+
+#include <moleculecontainer_parallel.hpp>
+
+int main(int argc, char* argv[])
+{
+    Kokkos::ScopeGuard guard(argc, argv);
+    //std::random_device rd;
+    //std::mt19937 gen(rd());
+
+    std::mt19937 gen(1984);
+    std::uniform_int_distribution<> dis(0, RAND_MAX);
+
+    MoleculeContainer container(10,12,gen,dis);
+    container.populateRandomly();
+    container.printData();
+    container.makeRandomHoles();
+    container.printData();
+    //container.compactor_onesweep();
+    //container.compactor_twosweep();
+    //container.compactor_pullback();
+    container.compactor_onesweep_noOrder();
+    return 0;
+}
---
+++ moleculecontainer_parallel.hpp
@@ -0,0 +1,209 @@
+#include <random>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+#include <Kokkos_Core.hpp>
+
+class MoleculeContainer
+{
+public:
+    MoleculeContainer(int numCells, int cellSize, std::mt19937 gen, std::uniform_int_distribution<> dis) : _numCells(numCells), _cellSize(cellSize), _gen(gen), 
+        _dis(dis), _moleculeData("moleculeData", numCells, cellSize) {}
+
+    void grow(int cellSize)
+    {
+        assert(_cellSize <= cellSize);
+        _cellSize = cellSize;
+        Kokkos::resize(_moleculeData,_numCells, _cellSize);
+    }
+
+    void printData() const
+    {
+        std::cout << "Container contents: " << std::endl;
+        for (size_t i = 0; i < _numCells; i++)
+        {
+            std::cout << "Cell #" << i << ": " ;
+            for (size_t j = 0; j < _cellSize; j++)
+            {
+                std::cout << _moleculeData(i,j) << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    void populateRandomly()
+    {
+        //not in parallel to make sure the same rows have same data
+        for (size_t i = 0; i < _numCells; i++)
+        {
+            for (size_t j = 0; j < _cellSize; j++)
+            {
+                _moleculeData(i,j) = _dis(_gen) % 9 + 1;
+            }
+        }
+    }
+
+    void makeRandomHoles()
+    {
+        int numHoles = _dis(_gen) % (_numCells*_cellSize);
+        std::vector<int> allCoords(_numCells*_cellSize);
+        std::iota(allCoords.begin(), allCoords.end(), 0);
+        std::shuffle(allCoords.begin(), allCoords.end(),_gen);
+        for (size_t i = 0; i < numHoles; i++)
+        {
+            _moleculeData(allCoords[i]/_cellSize, allCoords[i]%_cellSize) = 0;
+        }
+        std::cout << numHoles << " holes created!" << std::endl;
+    }
+
+    void compactor_onesweep()
+    {
+        Kokkos::View<int**> containerCopy("copy", _numCells, _cellSize);
+        Kokkos::deep_copy(containerCopy, _moleculeData);
+        Kokkos::parallel_for(_numCells, KOKKOS_LAMBDA(const unsigned int i)
+        {
+            int j = 0;
+            int lastHole = -1;
+            //find first hole
+            for (; j < _cellSize; j++)
+            {
+                if(containerCopy(i,j) == 0)
+                    break;
+            }
+            lastHole = j;
+            //iterate first hole onwards
+            for (int j = lastHole; j < _cellSize; j++)
+            {
+                if(containerCopy(i,j) != 0)
+                {
+                    containerCopy(i, lastHole) = containerCopy(i,j);
+                    containerCopy(i,j) = 0;
+                    while (containerCopy(i,lastHole) != 0 && lastHole < j) lastHole++;
+                }
+            }
+        }); //kokkos parallel for
+        std::cout << "Data compacted with onesweep! Data:" << std::endl;
+        for (size_t i = 0; i < _numCells; i++)
+        {
+            std::cout << "Cell #" << i << ": " ;
+            for (size_t j = 0; j < _cellSize; j++)
+            {
+                std::cout << containerCopy(i,j) << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    void compactor_twosweep()
+    {
+        Kokkos::View<int**> containerCopy("copy", _numCells, _cellSize);
+        Kokkos::deep_copy(containerCopy, _moleculeData);
+        Kokkos::parallel_for(_numCells, KOKKOS_LAMBDA(const unsigned int i)
+        {
+            int shiftAmt[_cellSize];
+            int curShift = 0;
+            for (int j = 0; j < _cellSize; j++)
+            {
+                shiftAmt[j] = curShift;
+                if(containerCopy(i,j) == 0)
+                {
+                    curShift++;
+                }
+            }
+            for (int j = 0; j < _cellSize; j++)
+            {
+                if(containerCopy(i,j) != 0 && shiftAmt[j] != 0)
+                {
+                    containerCopy(i, j-shiftAmt[j]) = containerCopy(i,j);
+                    containerCopy(i,j) = 0;
+                }
+            }
+        }); //kokkos parallel for
+        std::cout << "Data compacted with twosweep! Data:" << std::endl;
+        for (size_t i = 0; i < _numCells; i++)
+        {
+            std::cout << "Cell #" << i << ": " ;
+            for (size_t j = 0; j < _cellSize; j++)
+            {
+                std::cout << containerCopy(i,j) << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    void compactor_pullback()
+    {
+        Kokkos::View<int**> containerCopy("copy", _numCells, _cellSize);
+        Kokkos::deep_copy(containerCopy, _moleculeData);
+        Kokkos::parallel_for(_numCells, KOKKOS_LAMBDA(const unsigned int i)
+        {
+            int sourceIdx[_cellSize];
+            int sourceIdxIdx = 0;
+            for (int j = 0; j < _cellSize; j++)
+            {
+                if(containerCopy(i,j) != 0)
+                {
+                    sourceIdx[sourceIdxIdx++] = j;
+                }
+            }
+            for (int j = 0; j < _cellSize; j++)
+            {
+                if(j < sourceIdxIdx)
+                    containerCopy(i, j) = containerCopy(i,sourceIdx[j]);
+                else
+                    containerCopy(i, j) = 0;
+            }
+        }); //kokkos parallel for
+        std::cout << "Data compacted with pullback! Data:" << std::endl;
+        for (size_t i = 0; i < _numCells; i++)
+        {
+            std::cout << "Cell #" << i << ": " ;
+            for (size_t j = 0; j < _cellSize; j++)
+            {
+                std::cout << containerCopy(i,j) << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    void compactor_onesweep_noOrder()
+    {
+        Kokkos::View<int**> containerCopy("copy", _numCells, _cellSize);
+        Kokkos::deep_copy(containerCopy, _moleculeData);
+        //Kokkos::RangePolicy<Kokkos::Schedule<Kokkos::Static>> rp(0, _numCells, Kokkos::ChunkSize(_cellSize));
+        Kokkos::parallel_for(_numCells, KOKKOS_LAMBDA(const unsigned int i)
+        {
+            int j = 0, k = _cellSize - 1;
+            while(j < k)
+            {
+                //find first hole on left side
+                while(j < _cellSize && containerCopy(i,j) != 0) j++;
+                //find first data on right side
+                while(k > -1 && containerCopy(i,k) == 0) k--;
+                if(k <= j) break;
+                containerCopy(i,j) = containerCopy(i,k);
+                containerCopy(i,k) = 0;
+            }
+        }); //kokkos parallel for
+        std::cout << "Data compacted with onesweep_noOrder! Data:" << std::endl;
+        for (size_t i = 0; i < _numCells; i++)
+        {
+            std::cout << "Cell #" << i << ": " ;
+            for (size_t j = 0; j < _cellSize; j++)
+            {
+                std::cout << containerCopy(i,j) << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+
+private:
+    int _numCells;
+    int _cellSize;
+    std::mt19937 _gen;
+    std::uniform_int_distribution<> _dis;
+    Kokkos::View<int**> _moleculeData;
+};
